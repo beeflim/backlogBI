ce2e25553ffd967e4f6576acaa3c9a357d238418
beeflim <*****@users.noreply.github.com>
2019-03-28 14:26:27 +0900

    [modified] create line chart

--- pages/progress_chart.vue
+++ pages/progress_chart.vue
@@ -1,11 +1,11 @@
 <template>
-  <section class="container">
+  <section class="line-container">
     <div>
-      <h1 class="title" @click="getIssueData()">
-        {{ $store.state.projectName }} - {{ $store.state.milestoneName }}<br>{{ $store.state.chartName }}
+      <h1 class="line-title">
+        [{{ $store.state.chartName }}] {{ $store.state.projectName }} - {{ $store.state.milestoneName }}
       </h1>
-      <div>
-        <LineChart :data="lineChartData()" :options="lineOptions()"></LineChart>
+      <div style="width: 90%;">
+        <LineChart :data="writeLineChart()" :options="lineOptions()"></LineChart>
       </div>
     </div>
   </section>
@@ -20,60 +20,72 @@
     components: {
       AppLogo, LineChart
     },
+    props: {
+      labels: [],
+      estimatedData: [],
+      finishedData: []
+    },
     async mounted() {
-      let issueData = this.getIssueData();
+      let issueData = await this.getAllIssueData();
       issueData = this.format(issueData);
-
-      //データをチャートように成型する
-      //チャートを表示する
     },
     methods: {
-      lineChartData: function () {
+      writeLineChart: function () {
+
         return {
-          labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+          labels: this.$props.labels,
           datasets: [
             {
-              label: "Prime and Fibonacci",
-              fillColor: "rgba(220,220,220,0.2)",
-              strokeColor: "rgba(220,220,220,1)",
-              pointColor: "rgba(220,220,220,1)",
-              pointStrokeColor: "#fff",
-              pointHighlightFill: "#fff",
-              pointHighlightStroke: "rgba(220,220,220,1)",
-              data: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+              label: "完了 工数(時)",
+              borderColor:'rgba(86,167,100,1)',
+              backgroundColor:'rgba(86,167,100,0.1)',
+              borderWidth:'3',
+              pointBackgroundColor:'rgba(186,267,200,1)',
+              pointBorderColor:'rgba(86,167,100,1)',
+              data: this.$props.finishedData
             },
             {
-              label: "My Second dataset",
-              fillColor: "rgba(151,187,205,0.2)",
-              strokeColor: "rgba(151,187,205,1)",
-              pointColor: "rgba(151,187,205,1)",
-              pointStrokeColor: "#fff",
-              pointHighlightFill: "#fff",
-              pointHighlightStroke: "rgba(151,187,205,1)",
-              data: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+              label: "予定 工数(時)",
+              borderColor:'rgba(204,82,139,1)',
+              backgroundColor:'rgba(204,82,139,0.1)',
+              borderWidth:'3',
+              pointBackgroundColor:'rgba(1204,82,139,1)',
+              pointBorderColor:'rgba(204,82,139,1)',
+              data: this.$props.estimatedData
             }
           ]
         }
       }, lineOptions: function () {
         return {}
       },
-      getIssueData: async function () {
-        //TODO リファクタリング
-        let issueCount = this.$store.state.issueCount - 1;
+      getIssueData: async function (issueList) {
 
-        let issueList = {};
+        //一度に課題の数
         let getSize = 100;
-        let searchStartDate = '';
-        let url = `https://${localStorage.spaceName}.backlog.jp/api/v2/issues?apiKey=${localStorage.token}&projectId[]=${this.$store.state.projectId}&milestoneId[]=${this.$store.state.milestoneId}&count=${getSize}${searchStartDate}&sort=startDate&order=asc`;
+
+        // 開始日が最新の物を取得する
+        let dueDate = '';
+        if (issueList) {
+          for (let key in issueList) {
+            dueDate = (!dueDate || issueList[key].dueDate > dueDate) ? issueList[key].dueDate : dueDate;
+          }
+        }
+
+        // 検索条件を設定する。
+        let searchStartDate = dueDate ? '&dueDateSince=' + dueDate.substring(0, 10) : '';
+
+        // チケット情報を取得するURLを作成する
+        let url = `https://${localStorage.spaceName}.backlog.jp/api/v2/issues?apiKey=${localStorage.token}&projectId[]=${this.$store.state.projectId}&milestoneId[]=${this.$store.state.milestoneId}&count=${getSize}${searchStartDate}&sort=dueDate&order=asc`;
+
+        // 取得したticket件数
+        let size = 0;
 
         await this.$axios.$get(url)
           .then((res) => {
-            let size = 0;
             res.forEach(issueData => {
               issueList[issueData['issueKey']] = issueData;
               size++;
             });
-            issueCount -= size;
           })
           .catch((e => {
             this.$message({
@@ -83,38 +95,94 @@
             console.log(e);
           }));
 
+        return size;
+      },
+      getAllIssueData: async function () {
+        // 課題の件数
+        let issueCount = this.$store.state.issueCount;
+
+        // 課題のリスト
+        let issueList = {};
+
         while (issueCount > 0) {
-          // 次の結果の条件を設定する
-          let startDate = '';
-          for (let key in issueList) {
-            startDate = (!startDate || issueList[key].startDate > startDate) ? issueList[key].startDate : startDate;
-          }
-          searchStartDate = startDate ? '&startDateSince=' + startDate.substring(0, 10) : '';
-
-          url = `https://${localStorage.spaceName}.backlog.jp/api/v2/issues?apiKey=${localStorage.token}&projectId[]=${this.$store.state.projectId}&milestoneId[]=${this.$store.state.milestoneId}&count=${getSize}${searchStartDate}&sort=startDate&order=asc`;
-
-          await this.$axios.$get(url)
-            .then((res) => {
-              let size = 0;
-              res.forEach(issueData => {
-                issueList[issueData['issueKey']] = issueData;
-                size++;
-              });
-              issueCount -= size;
-            })
-            .catch((e => {
-              this.$message({
-                message: '課題を取得できませんでした',
-                type: 'warning'
-              });
-              console.log(e);
-            }));
+          let fetchSize = await this.getIssueData(issueList);
+          issueCount -= fetchSize;
         }
 
         return issueList;
       },
+      /**
+       * chartようにデータを成型する
+       * @param issueData APIで取得した課題データ
+       */
       format: function (issueData) {
-//TODO 実装
+        let labels = [];
+
+        // 予定データ
+        let estimatedData = [];
+        let estimatedSize = 0;
+
+        // 完了データ
+        let finishedData = [];
+        let finishSize = 0;
+
+        //完了日が未設定野場合
+        let unSetestimatedSize = 0;
+        let unSetfinishSize = 0;
+        let hasUnSetData = false;
+
+        Object.keys(issueData).forEach(function (key) {
+          // 完了日が設定されている場合
+          if (issueData[key].dueDate) {
+            // ラベルの設定
+            let hasLabel = false;
+            let labelName = issueData[key].dueDate.substring(5, 10).replace(/-/g, '/')
+
+            if (labels.indexOf(labelName) >= 0){
+              hasLabel = true;
+            }else{
+              hasLabel = false;
+              labels.push(labelName );
+            }
+
+
+            // 予定工数を登録
+            estimatedSize += issueData[key].estimatedHours;
+            if(hasLabel){
+              estimatedData[estimatedData.length -1] = Math.round(estimatedSize * 100) / 100;
+            }else {
+              estimatedData.push(Math.round(estimatedSize * 100) / 100);
+            }
+
+            // 完了工数を登録
+            finishSize += issueData[key].actualHours;
+            if (issueData[key].status.name === '完了') {
+              if(hasLabel){
+                finishedData[finishedData.length -1] = finishSize;
+              }else {
+                finishedData.push(Math.round(finishSize * 100) / 100);
+              }
+            }
+          } else {
+            // 未設定の時
+            unSetestimatedSize += issueData[key].estimatedHours;
+            unSetfinishSize += issueData[key].actualHours;
+            hasUnSetData = true;
+          }
+
+        });
+
+        // 未設定がある場合
+        if (hasUnSetData) {
+          labels.push('未設定');
+          estimatedData.push(Math.round(unSetestimatedSize + estimatedSize * 100) / 100);
+          finishedData.push(Math.round(unSetfinishSize + finishSize * 100) / 100);
+        }
+
+        this.$props.labels = labels;
+        this.$props.estimatedData = estimatedData;
+        this.$props.finishedData = finishedData;
+
       }
     }
   }
@@ -122,21 +190,19 @@
 </script>
 
 <style>
-  .container {
-    min-height: 100vh;
+  .line-container {
     display: flex;
     justify-content: center;
     align-items: center;
     text-align: center;
   }
 
-  .title {
+  .line-title {
     font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
     display: block;
     font-weight: bold;
-    font-size: 40px;
+    font-size: 30px;
     color: #35495e;
     letter-spacing: 1px;
-    margin: 30px 0;
   }
 </style>
